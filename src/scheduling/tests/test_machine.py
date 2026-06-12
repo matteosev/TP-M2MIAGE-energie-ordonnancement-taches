2649c7aa64fac3df342f80100a495c5e38625cf0
Matteo SEVERINI <severini.matt@gmail.com>
2026-06-12 09:09:18 +0200

    fix test_machine.py

--- src/scheduling/tests/test_machine.py
+++ src/scheduling/tests/test_machine.py
@@ -26,9 +26,11 @@ class TestMachine(unittest.TestCase):
         machine = inst.machines[0]
         
         # Simulation d'un cycle d'allumage/extinction
-        machine.start_times = [10, 50]
-        machine.stop_times = [30, 80]
-        
+        machine.start_times.append(10)
+        machine.start_times.append(50)
+        machine.stop_times.append(30)
+        machine.stop_times.append(80)
+
         # (30 - 10) + (80 - 50) = 20 + 30 = 50
         self.assertEqual(machine.working_time, 50, 'Le working_time doit être la somme des durées de fonctionnement actives')
 
